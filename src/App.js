eaf18f3c4df76d558f02efeee4d820b759ac89fe
abhishekkashyap98 <abhishek61010@gmail.com>
2025-05-22 09:43:31 +0530

    fix:some code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
 import Dashboard from './screens/Dashboard';
 import About from './screens/About';
-
 function App() {
   return (
     <Router>
-      <div className="App">
-        <main style={{ padding: '20px' }}>
           <Routes>
             <Route path="/" element={<Dashboard />} />
-            <Route path="/about" element={<About />} />
+             <Route path="/about" element={<About />} />
           </Routes>
-        </main>
-      </div>
     </Router>
   );
 }
--- src/screens/Dashboard/index.jsx
+++ src/screens/Dashboard/index.jsx
@@ -1,24 +1,186 @@
 import React from "react";
-import Header from "../../components/Header";
-import Topbar from "../../components/Topbar";
-import TrackerBoard from "../../components/TrackerBoard";
-import Inbox from "../../components/Inbox";
+import { Link } from "react-router-dom";
+
 
 export default function Dashboard() {
   return (
-    <div className="flex h-screen bg-[#0f0f10] text-white">
-      <Header />
-      <div className="flex flex-col flex-1 overflow-hidden">
-        <Topbar />
-        <div className="flex flex-1 overflow-hidden">
-          <div className="flex-1 overflow-auto p-4">
-            <TrackerBoard />
+<>  
+<div className="main-banner container-fluig border-0" style={{ minHeight: '100vh' }}>
+    <nav className="navbar navbar-expand-md bg-body-tertiary">
+      <div className="container">
+        <img src="../../assets/images/logo.png" alt="" className="img-fluid" />
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarScroll"
+          aria-controls="navbarScroll"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarScroll">
+          <ul
+            className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll ps-5"
+            style={{ '--bs-scroll-height': '100px' }}
+          >
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to={""}
+                >Pricing</Link>
+            </li>
+
+            <li className="nav-item dropdown">
+              <Link
+                className="nav-link dropdown-toggle"
+                to={""}
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Resources
+              </Link>
+              <ul className="dropdown-menu">
+                <li><Link className="dropdown-item" to={""}>Action</Link></li>
+                <li><Link className="dropdown-item" to={""}>Another action</Link></li>
+                <li><hr className="dropdown-divider" /></li>
+                <li>
+                  <Link className="dropdown-item" to={""}>Something else here</Link>
+                </li>
+              </ul>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Community
+              </a>
+              <ul className="dropdown-menu">
+                <li><Link className="dropdown-item" to={""}>Action</Link></li>
+                <li><Link className="dropdown-item" to={""}>Another action</Link></li>
+                <li><hr className="dropdown-divider" /></li>
+                <li>
+                  <Link className="dropdown-item" to={""}>Something else here</Link>
+                </li>
+              </ul>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to={""}
+                >Download</Link >
+            </li>
+          </ul>
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to={""}
+                >Get Started</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to={""}
+                >Sign In</Link>
+            </li>
+          </ul>
+       
+        </div>
+      </div>
+    </nav>
+ </div>  
+    <section class="overflow-hidden">
+      <div class="py-60 position-realtive">
+        <div class="container">
+          <div class="video-wrapper">
+            <div class="background-image-wrapper">
+              <video
+                class="position-absolute inset-0"
+                width="1920"
+                height="1185"
+                autoplay=""
+                loop=""
+                playsinline=""
+                style={{ opacity: 1 }}
+              >
+                <source src="./assets/images/waves.mp4" type="video/mp4" />
+              </video>
+            </div>
           </div>
-          <div className="w-[350px] border-l border-gray-800 overflow-auto">
-            <Inbox />
+        </div>
+      </div>
+    </section>
+
+    <section class="custom-section">
+      <div class="custom-container">
+        <div class="custom-text-wrapper">
+          <h2 class="custom-heading">Join the Movement</h2>
+          <p class="custom-paragraph">
+            Unlock the future of productivity with Huly.<br />
+            Remember, this journey is just getting started.
+          </p>
+          <div class="custom-button-wrapper">
+            {/* <!-- <div class="custom-button-wrapper"> --> */}
+            <div class="custom-border-glow"></div>
+            <div class="custom-border-glow custom-border-glow-reverse"></div>
+
+            <a class="custom-button" href="/signup">
+              <div class="custom-button-bg">
+                <div class="custom-glow-circle"></div>
+                <div class="custom-glow-gradient"></div>
+              </div>
+              <span>Start now</span>
+            </a>
+            {/* <!-- </div> --> */}
+
+            <a
+              class="custom-btn-slack"
+              href="https://huly.link/slack"
+              target="_blank"
+            >
+              <img
+                src="/_next/static/media/68af230db56f9e75d64156987f7e1b09.svg"
+                alt=""
+              />
+              <span>Join our Slack</span>
+            </a>
+          </div>
+        </div>
+
+        <img
+          class="custom-image-desktop"
+          src="./images/image (12).png"
+          alt=""
+        />
+        {/* <!-- <img
+          class="custom-image-mobile"
+          src="/_next/static/media/cta-illustration-mobile.621dcd29.jpg"
+          alt=""
+        /> --> */}
+
+        <div class="custom-video-container">
+          <video autoplay loop muted playsinline>
+            <source src="./images/clock.mp4" type="video/mp4" />
+            {/* <!-- <source
+              src="/videos/cta/clock.webm?updated=20240531154316"
+              type="video/webm"
+            /> --> */}
+          </video>
+        </div>
+      </div>
+    </section>
+    <div class="bottom-bar">
+      <div class="container">
+        <div class="d-flex justify-content-between">
+          <p class="copyrightText">
+            Copyright © 2025 Huly Labs. All rights reserved.
+          </p>
+          <div class="d-flex justify-content-center gap-4 align-items-center">
+            <p class="copyrightText">Terms of Service</p>
+            <p class="copyrightText">Privacy Policy</p>
           </div>
         </div>
       </div>
     </div>
+    </>
   );
 }
